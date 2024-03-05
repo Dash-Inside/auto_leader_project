0ff76d183119622485cc86368d98ef60b305ecea
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-03-05 09:49:11 +0300

    feat(view): add _HeaderButton and replace CustomToggleButton with them

--- lib/src/presentation/content/navigation_bar_view.dart
+++ lib/src/presentation/content/navigation_bar_view.dart
@@ -1,37 +1,96 @@
-import 'package:auto_leader_project/src/presentation/widgets/custom_toggle_button.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:auto_leader_project/src/presentation/style/color_style.dart';
+import 'package:auto_leader_project/src/presentation/style/text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 /// Sections nav panel.
 class NavigationBarView extends StatelessWidget {
   /// Returns an instance of [NavigationBarView].
-  static const double svgSize = 48.0;
   const NavigationBarView({super.key});
+
+  static const double _svgSize = 48.0;
+
+  static const List<String> _viewSections = ['О Нас', 'Стоимость', 'Акции'];
 
   @override
   Widget build(BuildContext context) {
     final Color onBackground = Theme.of(context).colorScheme.onBackground;
     final TextStyle title = Theme.of(context).textTheme.titleMedium!;
-    return Scaffold(
-      //убрать нахуй
-      body: Row(
-        children: [
-          SvgPicture.asset(
-            'assets/icons/logo.svg',
-            height: svgSize,
-            width: svgSize,
+
+    return Row(
+      children: [
+        SvgPicture.asset(
+          'assets/icons/logo.svg',
+          height: _svgSize,
+          width: _svgSize,
+        ),
+        const SizedBox(
+          width: 32.0,
+        ),
+        Text(
+          'АвтоЛидер',
+          style: getTitleTextStyle(context),
+        ),
+        const Spacer(),
+        // const CustomToggleButton(),
+        Wrap(
+          spacing: 32.0,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: _viewSections.map(
+            (_) {
+              return _HeaderButton(
+                text: _,
+                highlighted: _viewSections.first == _,
+                onPressed: () {
+                  debugPrint('$_ Pressed');
+                },
+              );
+            },
+          ).toList(),
+        ),
+      ],
+    );
+  }
+}
+
+class _HeaderButton extends StatelessWidget {
+  const _HeaderButton({
+    required this.text,
+    required this.highlighted,
+    this.onPressed,
+  });
+
+  final String text;
+  final bool highlighted;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    const BorderRadius radius = BorderRadius.all(Radius.circular(8));
+
+    final BoxDecoration defaultDecoratedBox = BoxDecoration(
+      color: getBackgroundColor(context),
+      borderRadius: radius,
+    );
+
+    final BoxDecoration highlightedDecoratedBox = BoxDecoration(
+      color: getPrimaryColor(context),
+      borderRadius: radius,
+    );
+
+    return InkWell(
+      onTap: onPressed,
+      borderRadius: radius,
+      child: Container(
+        padding: highlighted ? const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0) : null,
+        decoration: highlighted ? highlightedDecoratedBox : defaultDecoratedBox,
+        child: Text(
+          text,
+          style: getLightTextStyle(
+            context,
+            color: highlighted ? getBackgroundColor(context) : getOnBackgroundColor(context),
           ),
-          const SizedBox(
-            width: 32.0,
-          ),
-          Text(
-            'АвтоЛидер',
-            style: title.copyWith(color: onBackground),
-          ),
-          const Spacer(),
-          const CustomToggleButton(),
-        ],
+        ),
       ),
     );
   }

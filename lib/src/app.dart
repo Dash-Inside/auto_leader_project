850a01680f96a32e4e86430e5e399a427b140261
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-03-05 09:47:56 +0300

    feat(style): remove styles from default context theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:auto_leader_project/src/presentation/content/about_us_view.dart';
 import 'package:auto_leader_project/src/presentation/content/navigation_bar_view.dart';
+import 'package:auto_leader_project/src/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 /// Main application of the site.
@@ -14,40 +15,12 @@ class App extends StatelessWidget {
       title: 'Автошкола АвтоЛидер',
       theme: ThemeData(
         useMaterial3: true,
-        textTheme: const TextTheme(
-          titleMedium: TextStyle(
-            fontSize: 36.0,
-            fontWeight: FontWeight.w700,
-            fontFamily: 'Nunito',
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.w400,
-            fontFamily: 'Nunito',
-          ),
-          displayMedium: TextStyle(
-            fontSize: 48.0,
-            fontWeight: FontWeight.w300,
-            fontFamily: 'Nunito',
-          ),
-          bodySmall: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.w300,
-            fontFamily: 'Nunito',
-          ),
-          titleSmall: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.w400,
-            fontFamily: 'Nunito',
-          ),
-        ),
         colorScheme: const ColorScheme.light(
-          background: Color.fromRGBO(255, 255, 255, 1),
           primary: Color.fromRGBO(255, 51, 51, 1),
           onBackground: Color.fromRGBO(35, 44, 51, 1),
         ),
       ),
-      home: const NavigationBarView(),
+      home: const HomePage(),
     );
   }
 }

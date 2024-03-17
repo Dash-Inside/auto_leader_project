835dbeb5f6f9906e9b09db3e32aa1d81058d8aa6
ShySpyDie <92712724+ShySpyDie@users.noreply.github.com>
2024-03-18 02:36:23 +0300

    feat: add some changes at nav bar

--- lib/src/presentation/content/navigation_bar_view.dart
+++ lib/src/presentation/content/navigation_bar_view.dart
@@ -1,7 +1,7 @@
 import 'package:auto_leader_project/src/presentation/style/color_style.dart';
 import 'package:auto_leader_project/src/presentation/style/text_style.dart';
+import 'package:auto_leader_project/src/presentation/widgets/button_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 /// Sections nav panel.
 class NavigationBarView extends StatelessWidget {
@@ -11,27 +11,26 @@ class NavigationBarView extends StatelessWidget {
   /// Height of App Bar for construct pages with correct sizes.
   static const double expandedAppBarHeight = 88.0;
 
-  static const double _svgSize = 48.0;
-  static const List<String> _viewSections = ['О Нас', 'Стоимость', 'Акции'];
+  // static const double _svgSize = 48.0;
+  static const List<String> _viewSections = ['О Нас', 'Стоимость', 'Акции', 'Расписание', 'Мы на карте'];
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        SvgPicture.asset(
-          'assets/icons/logo.svg',
-          height: _svgSize,
-          width: _svgSize,
-        ),
-        const SizedBox(
-          width: 32.0,
-        ),
+        // SvgPicture.asset(
+        //   'assets/icons/logo.svg',
+        //   height: _svgSize,
+        //   width: _svgSize,
+        // ),
+        // const SizedBox(
+        //   width: 32.0,
+        // ),
         Text(
           'АвтоЛидер',
           style: getTitleTextStyle(context),
         ),
         const Spacer(),
-        // const CustomToggleButton(),
         Wrap(
           spacing: 32.0,
           crossAxisAlignment: WrapCrossAlignment.center,
@@ -47,6 +46,8 @@ class NavigationBarView extends StatelessWidget {
             },
           ).toList(),
         ),
+        const SizedBox(width: 32.0),
+        const ButtonWidget(text: 'Записаться'),
       ],
     );
   }
@@ -65,30 +66,34 @@ class _HeaderButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const BorderRadius radius = BorderRadius.all(Radius.circular(8));
-
     final BoxDecoration defaultDecoratedBox = BoxDecoration(
-      color: getBackgroundColor(context),
-      borderRadius: radius,
+      border: Border(bottom: BorderSide(color: getBackgroundColor(context))),
     );
 
     final BoxDecoration highlightedDecoratedBox = BoxDecoration(
-      color: getPrimaryColor(context),
-      borderRadius: radius,
+      border: Border(bottom: BorderSide(color: getPrimaryColor(context), width: 1.3)),
+    );
+
+    final TextStyle defaultTextStyle = getLightTextStyle(
+      context,
+      color: getOnBackgroundColor(context),
+    );
+
+    final TextStyle highlightedTextStyle = getRegularTextStyle(
+      context,
+      color: getOnBackgroundColor(context),
     );
 
     return InkWell(
+      hoverColor: Colors.transparent,
+      overlayColor: const MaterialStatePropertyAll(Colors.transparent),
       onTap: onPressed,
-      borderRadius: radius,
       child: Container(
-        padding: highlighted ? const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0) : null,
+        padding: highlighted ? const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0) : null,
         decoration: highlighted ? highlightedDecoratedBox : defaultDecoratedBox,
         child: Text(
           text,
-          style: getLightTextStyle(
-            context,
-            color: highlighted ? getBackgroundColor(context) : getOnBackgroundColor(context),
-          ),
+          style: highlighted ? highlightedTextStyle : defaultTextStyle,
         ),
       ),
     );

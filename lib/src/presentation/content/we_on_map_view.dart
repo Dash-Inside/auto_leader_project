9bd8e2705d2de374c2e8442b82eee81be6433a77
ShySpyDie <92712724+ShySpyDie@users.noreply.github.com>
2024-03-21 19:11:15 +0300

    feat: update map button

--- lib/src/presentation/content/we_on_map_view.dart
+++ lib/src/presentation/content/we_on_map_view.dart
@@ -21,77 +21,108 @@ class WeOnMapView extends StatelessWidget {
           style: getSubtitleTextStyle(context),
         ),
         const SizedBox(height: 32.0),
-        Row(
+        Stack(
           children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Наш адрес:',
-                    style: getSubtitleTextStyle(
-                      context,
-                      color: getPrimaryColor(context),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(32.0)),
+              child: SizedBox(
+                height: sizedBoxHeight,
+                child: SfMaps(
+                  layers: [
+                    MapTileLayer(
+                      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      initialFocalLatLng: const MapLatLng(
+                        56.4006,
+                        38.7103,
+                      ),
+                      zoomPanBehavior: MapZoomPanBehavior(
+                        maxZoomLevel: 20,
+                        zoomLevel: 15.5,
+                        // enableMouseWheelZooming: true,
+                        enableDoubleTapZooming: true,
+                      ),
+                      initialMarkersCount: 2,
+                      markerBuilder: (BuildContext context, int index) {
+                        return MapMarker(
+                          latitude: dataOfLocations[index].latitude,
+                          longitude: dataOfLocations[index].longitude,
+                          child: const Icon(
+                            Icons.location_pin,
+                            size: 48.0,
+                            color: Colors.red,
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                  Text(
-                    'г. Александров, ул. Ленина, д. 13, корп. 5, офис 4',
-                    style: getLightTextStyle(context),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            Container(
+              height: 80.0,
+              alignment: Alignment.bottomCenter,
+              margin: const EdgeInsets.all(32.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(40.0),
+                ),
+              ),
+              child: Row(
                 children: [
-                  Text(
-                    'Адрес автодрома:',
-                    style: getSubtitleTextStyle(
-                      context,
-                      color: getPrimaryColor(context),
+                  Expanded(
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: getPrimaryColor(context),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(40.0),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Наш адрес:',
+                            style: getRegularTextStyle(context, color: Colors.white),
+                          ),
+                          Text(
+                            'г. Александров, ул. Ленина, д. 13, корп. 5, офис 4',
+                            style: getLightTextStyle(context, color: Colors.white),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  Text(
-                    'г. Алескиндров, ул. Двориковское шоссе 15 (район Геологи)',
-                    style: getLightTextStyle(context),
+                  Expanded(
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(40.0),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Адрес автодрома:',
+                            style: getRegularTextStyle(
+                              context,
+                              color: getPrimaryColor(context),
+                            ),
+                          ),
+                          Text(
+                            'г. Алескиндров, ул. Двориковское шоссе 15 (район Геологи)',
+                            style: getLightTextStyle(context),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
           ],
-        ),
-        SizedBox(
-          height: sizedBoxHeight,
-          child: SfMaps(
-            layers: [
-              MapTileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                initialFocalLatLng: const MapLatLng(
-                  56.4006,
-                  38.7103,
-                ),
-                zoomPanBehavior: MapZoomPanBehavior(
-                  maxZoomLevel: 20,
-                  zoomLevel: 15.5,
-                  // enableMouseWheelZooming: true,
-                  enableDoubleTapZooming: true,
-                ),
-                initialMarkersCount: 2,
-                markerBuilder: (BuildContext context, int index) {
-                  return MapMarker(
-                    latitude: dataOfLocations[index].latitude,
-                    longitude: dataOfLocations[index].longitude,
-                    child: const Icon(
-                      Icons.location_pin,
-                      size: 48.0,
-                      color: Colors.red,
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
         ),
       ],
     );
